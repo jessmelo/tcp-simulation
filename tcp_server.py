816d7293070b2327e7a992fbe70f5a336fb601bb
jessmelo <jessmelo@usp.br>
2023-01-03 22:54:09 -0300

    Adicionando controle de congestionamento

--- tcp_server.py
+++ tcp_server.py
@@ -17,15 +17,16 @@ received_messages = {}
 # Inicializa o tamanho da janela de congestionamento
 cwnd = 1
 
+# Set list to store congestion window sizes
+cwnd_sizes = []
+
 # Define o slow start threshold inicial
 ssthresh = 64
 
-# Inicializa o tamanho máximo da janela de recebimento
-MAX_WINDOW_SIZE = 10
-
-# Inicializa a variável de tempo total e pacotes recebidos
+# Inicializa a variável de tempo totat, pacotes recebidos e pacotes perdidos
 total_time = 0
 total_packets = 0
+total_packets_lost = 0
 
 while True:
     # Define tempo de inicio antes de um acknowledgment
@@ -37,8 +38,12 @@ while True:
     # Simulando perda de pacote
     if random.random() < 0.1:
         print("Pacote perdido! :(")
-        cwnd = cwnd - 1  # Decrementa janela de congestionamento
+        cwnd = cwnd // 2  # Decrementa janela de congestionamento pela metade
+        cwnd_sizes.append(cwnd)
+        total_packets_lost = total_packets_lost + 1
         continue
+    else:
+        cwnd += 1  # Incrementa janela de congestionamento em 1
 
     # Decodando mensagem recebida
     msg = msg.decode()
@@ -50,6 +55,7 @@ while True:
         end_time = time.time()  # Guarda o tempo final depois de um acknowledgment
         elapsed_time = end_time - start_time  # Calcula o tempo da transmissao
         total_time += elapsed_time  # Guarda tempo total de transmissao
+        servidor.close()
         break
 
     # Verifica se é uma retransmissao
@@ -59,6 +65,7 @@ while True:
         print(ack_num)
         servidor.sendto(str(ack_num).encode(), addr)
 
+        cwnd_sizes.append(cwnd)
         total_packets += 1
     else:
         # Se é uma nova mensagem, guarda no dicionário
@@ -73,6 +80,7 @@ while True:
         clientMsg = "Número de sequência do pacote: {}".format(msg[:5])
         clientIP = "Endereço IP do cliente: {}".format(addr)
 
+        cwnd_sizes.append(cwnd)
         total_packets += 1
 
         end_time = time.time()  # Guarda o tempo final depois de um acknowledgment
@@ -83,11 +91,12 @@ while True:
         print(clientMsg)
         print(clientIP)
 
-
 # Calcula o tempo médio de transmissão
 avg_time = total_time / total_packets
 
 # Resultados finais
 print(f'Total de pacotes transmitidos: {total_packets}')
+print(f'Total de pacotes perdidos: {total_packets_lost}')
 print(f'Tempo total de transmissão: {total_time} segundos')
 print(f'Tempo médio de transmissão: {avg_time} segundos')
+print(f'Janelas de congestixonamento: {cwnd_sizes}')
